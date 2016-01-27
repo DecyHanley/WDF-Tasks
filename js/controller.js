0ecad5668b7fd16faff2b0d2878a080015ce44c1
Declan Hanley <decy.hanley@gmail.com>
2016-01-27 12:37:48 +0000

    Task Complete... I Think...

--- js/controller.js
+++ js/controller.js
@@ -3,18 +3,22 @@ var w2MusicApp = angular.module('w2MusicApp',[]);
 // Add a controller to the module.
 w2MusicApp.controller('w2maCtrl', function($scope){
    this.doReverse = false;
-   this.genres = ['Glam Rock','Heartline Rock',''];
+   this.albums = ['The Man who sold the World','Born in the US'];
+   this.genres = ['Glam Rock','Heartland Rock'];
+   this.albumYears = ['1970','1984'];
 
    this.artists = [
-       {
-           name: "David Bowie",
-           album: "The Man who sold the World",
-           genre: "Glam Rock"
-       },
-       {
-           name: "Bruce Springsteen",
-           album: "Born in the USA"
-           genre: "Heartland Rock"
-       }
+     {
+         name: "David Bowie",
+         album: "The Man who sold the World",
+         genre: "Glam Rock",
+         albumYear: "1970"
+     },
+     {
+         name: "Bruce Springsteen",
+         album: "Born in the USA",
+         genre: "Heartland Rock",
+         albumYear: "1984"
+     }
    ];
 });
